572a4db3dbcc67d79308c2e2123aed4c21ec8940
f4Mythical <grynka.adam@gmail.com>
2025-11-18 08:29:08 +0100

    pobralem buttony do mainactivity

--- app/src/main/java/com/example/zadanie/MainActivity.java
+++ app/src/main/java/com/example/zadanie/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.zadanie;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+  private EditText TekstEmail;
+  private EditText TekstHaslo;
+  private Button ButttonZatwierdz;
+  private Button ButtonWygenerujHaslo;
+  private Button ButtonSprawdzMoc;
+  private TextView TekstDuzeLitery;
+  private TextView TekstMaleLitery;
+  private TextView TekstSpecjalne;
+  private TextView TekstCyfry;
+  private TextView TekstDlugosc;
+  private String haslo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +34,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TekstEmail = findViewById(R.id.editTextEmail);
+        TekstHaslo = findViewById(R.id.editTextPassword);
+        ButttonZatwierdz = findViewById(R.id.buttonRegister);
+        ButtonWygenerujHaslo = findViewById(R.id.buttonRandom);
+        ButtonSprawdzMoc = findViewById(R.id.buttonPower);
+        TekstDuzeLitery = findViewById(R.id.UpperCase);
+        TekstMaleLitery = findViewById(R.id.LowerCase);
+        TekstSpecjalne = findViewById(R.id.Special);
+        TekstCyfry = findViewById(R.id.Number);
+        TekstDlugosc= findViewById(R.id.Lenght);
+
+
+        ButttonZatwierdz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    }
+                }
+        );
     }
 }
