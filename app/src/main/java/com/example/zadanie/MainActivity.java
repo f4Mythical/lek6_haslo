1d89229b8d214b98397cb96ebef85279b7327c2b
Adamek303 <boy1234ksg@gmail.com>
2025-11-23 11:55:33 +0100

    zrobione z buttonem, zostalo jeszcze rodzielic to i moze wyglad

--- app/src/main/java/com/example/zadanie/MainActivity.java
+++ app/src/main/java/com/example/zadanie/MainActivity.java
@@ -190,56 +190,69 @@ TextPow = findViewById(R.id.textPow);
         ButtonWygenerujHaslo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText input = new EditText(MainActivity.this);
+                input.setInputType(InputType.TYPE_CLASS_NUMBER);
 
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Podaj długość hasła")
+                        .setView(input)
+                        .setPositiveButton("OK", (dialog, which) -> {
 
-                String all = duze + male + specjalne + liczby;
+                            String tekst = input.getText().toString();
+                            if (!tekst.isEmpty()) {
 
-                Random random = new Random();
+                                int dlugosc = Integer.parseInt(tekst);
 
-                ArrayList<Integer> ciag = new ArrayList<>();
-                for (int i = 0; i < 14; i++) {
-                    ciag.add(i);
-                }
+                                String all = duze + male + specjalne + liczby;
 
-                ArrayList<Integer> wyjatki = new ArrayList<>();
-                wyjatki.add(1); wyjatki.add(2); wyjatki.add(3); wyjatki.add(4);
+                                Random random = new Random();
 
+                                ArrayList<Integer> ciag = new ArrayList<>();
+                                for (int i = 0; i < dlugosc; i++) {
+                                    ciag.add(i);
+                                }
 
-                char[] wynik = new char[14];
+                                ArrayList<Integer> wyjatki = new ArrayList<>();
+                                wyjatki.add(1);
+                                wyjatki.add(2);
+                                wyjatki.add(3);
+                                wyjatki.add(4);
 
-                for (int x = 0; x < wyjatki.size(); x++) {
+                                char[] wynik = new char[dlugosc];
 
-                    int typ = wyjatki.get(x);
-                    int index = random.nextInt(ciag.size());
-                    int pozycja = ciag.get(index);
-                    ciag.remove(index);
+                                for (int x = 0; x < wyjatki.size(); x++) {
 
-                    switch (typ) {
-                        case 1:
-                            wynik[pozycja] = duze.charAt(random.nextInt(duze.length()));
-                            break;
+                                    int typ = wyjatki.get(x);
+                                    int index = random.nextInt(ciag.size());
+                                    int pozycja = ciag.get(index);
+                                    ciag.remove(index);
 
-                        case 2:
-                            wynik[pozycja] = male.charAt(random.nextInt(male.length()));
-                            break;
+                                    switch (typ) {
+                                        case 1:
+                                            wynik[pozycja] = duze.charAt(random.nextInt(duze.length()));
+                                            break;
+                                        case 2:
+                                            wynik[pozycja] = male.charAt(random.nextInt(male.length()));
+                                            break;
+                                        case 3:
+                                            wynik[pozycja] = specjalne.charAt(random.nextInt(specjalne.length()));
+                                            break;
+                                        case 4:
+                                            wynik[pozycja] = liczby.charAt(random.nextInt(liczby.length()));
+                                            break;
+                                    }
+                                }
 
-                        case 3:
-                            wynik[pozycja] = specjalne.charAt(random.nextInt(specjalne.length()));
-                            break;
+                                for (int pos : ciag) {
+                                    wynik[pos] = all.charAt(random.nextInt(all.length()));
+                                }
 
-                        case 4:
-                            wynik[pozycja] = liczby.charAt(random.nextInt(liczby.length()));
-                            break;
-                    }
-                }
-
-                for (int pos : ciag) {
-                    wynik[pos] = all.charAt(random.nextInt(all.length()));
-                }
-
-                String haslo = new String(wynik);
-
-                TekstZmien.setText(haslo);
+                                String haslo = new String(wynik);
+                                TekstZmien.setText(haslo);
+                            }
+                        })
+                        .setNegativeButton("Anuluj", null)
+                        .show();
             }
         });
 
